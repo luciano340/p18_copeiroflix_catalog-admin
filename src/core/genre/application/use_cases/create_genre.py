5a00e7f9e686224d76647cbc27a255ec38c5332c
Luciano Romão <lucianosouzaromao@gmail.com>
2024-09-01 10:08:52 -0300

    feat: add create genre usecase

---
+++ src/core/genre/application/use_cases/create_genre.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass, field
+import re
+from uuid import UUID
+from datetime import datetime
+
+from src.core.category.domain.category_repository_interface import CategoryRepositoryInterface
+from src.core.genre.application.use_cases.exceptions import InvalidGenre, RelatedCategoriesNotFound
+from src.core.genre.domain.genre import Genre
+from src.core.genre.domain.genre_repository_interface import GenreRepositoryInterface
+
+@dataclass
+class CreateGenreRequest:
+    name: str
+    categories_ids: set[UUID] = field(default_factory=set)
+    is_active: bool = True
+
+@dataclass
+class CreateGenreResponse:
+    id: UUID
+
+class CreateGenre:
+    def __init__(self, repository: GenreRepositoryInterface ,category_repository: CategoryRepositoryInterface) -> CreateGenreResponse:
+        self.repository = repository
+        self.category_repository = category_repository
+    
+    def execute(self, request: CreateGenreRequest) -> CreateGenreResponse:
+        categories_ids = {category.id for category in self.category_repository.list()}
+
+        if not request.categories_ids.issubset(categories_ids):
+            raise RelatedCategoriesNotFound(f"Categories ID not found: {request.categories_ids - categories_ids}")
+
+        try:
+            genre = Genre(
+                name=request.name,
+                is_active=request.is_active,
+                categories=request.categories_ids
+            )
+        except ValueError as err:
+            raise InvalidGenre(err)
+    
+        self.repository.save(genre)
+        return CreateGenreResponse(
+            id=genre.id
+        )
