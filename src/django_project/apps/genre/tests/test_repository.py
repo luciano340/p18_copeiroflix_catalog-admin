add6e68f2ec9081d113434d3ee3dea0bf5a27007
Luciano Romão <lucianosouzaromao@gmail.com>
2024-09-05 20:01:43 -0300

    chore: add all test for repository

--- src/django_project/apps/genre/tests/test_repository.py
+++ src/django_project/apps/genre/tests/test_repository.py
@@ -53,3 +53,78 @@ class TestRepositoryGenre:
         assert genre_model.categories.count() == 1
         assert genre_model.created_date.strftime("%Y-%m-%d %H:%M:%S") == "2024-05-04 04:04:04"
         assert genre_model.updated_date == None
+    
+    def test_delete_genre(self):
+        repository = DjangoORMGenreRepository()
+        genre = Genre(
+            name="Drama",
+            is_active=True,
+            categories={}
+        )
+
+        assert GenreORM.objects.count() == 0
+        repository.save(genre=genre)
+        assert GenreORM.objects.count() == 1
+        repository.delete_by_id(id=genre.id)
+        assert GenreORM.objects.count() == 0
+    
+    def test_get_genre_by_id(self):
+        repository = DjangoORMGenreRepository()
+        genre1 = Genre(
+            name="Drama",
+            is_active=True,
+            categories={}
+        )
+
+        genre2 = Genre(
+            name="Terror",
+            is_active=False,
+            categories={}
+        )
+        repository.save(genre=genre1)
+        repository.save(genre=genre2)
+        
+        genre_from_orm = repository.get_by_id(id=genre1.id)
+
+        assert isinstance(genre_from_orm, Genre)
+        assert genre_from_orm.id == genre1.id
+        assert genre_from_orm.name == genre1.name
+        assert genre_from_orm.is_active == genre1.is_active
+    
+    def test_get_all_genres(self):
+        repository = DjangoORMGenreRepository()
+        category_repository = DjangoORMCategoryRepository()
+
+        category = Category(name="Filme", description="Filmes")
+        category_repository.save(category=category)
+
+        genre1 = Genre(
+            name="Drama",
+            is_active=True,
+            categories={}
+        )
+
+        genre2 = Genre(
+            name="Terror",
+            is_active=False,
+            categories={category.id}
+        )
+
+        assert GenreORM.objects.count() == 0
+        repository.save(genre=genre1)
+        repository.save(genre=genre2)
+
+        assert GenreORM.objects.count() == 2
+
+        list_of_genres = repository.list()     
+
+        assert len(list_of_genres) == 2
+        assert list_of_genres[0].id == genre1.id
+        assert list_of_genres[0].name == genre1.name
+        assert list_of_genres[0].is_active == genre1.is_active
+        assert len(list_of_genres[0].categories) == 0
+        assert list_of_genres[1].id == genre2.id
+        assert list_of_genres[1].name == genre2.name
+        assert list_of_genres[1].is_active == genre2.is_active
+        assert len(list_of_genres[1].categories) == 1
+        assert list_of_genres[1].categories == {category.id}
